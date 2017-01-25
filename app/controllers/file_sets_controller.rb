aa10845b53f41d762483f3dd5477ada68f1e6c41
Ned Henry <ned.henry@gmail.com>
2017-01-24 16:14:46 -0800

    added access permissions for fileset streaming

--- app/controllers/file_sets_controller.rb
+++ app/controllers/file_sets_controller.rb
@@ -1,5 +1,7 @@
 
 #class FileSetsController < CurationConcerns::FileSetsController
 class FileSetsController 
+  skip_before_action :authenticate_user!, :only => [:show,:citation, :stats,  :embed, :master, :variant]
+  skip_authorize_resource :only => [:show,:citation, :stats,  :embed, :master, :variant]
   include HydraHls::FileSetsControllerBehavior  
 end
