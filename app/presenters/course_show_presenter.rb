f448dd778223d99538db36368724cbe9f0daf14e
Ned Henry <ned.henry@gmail.com>
2017-03-09 09:50:44 -0800

    fixed lecture order in course playlist

--- app/presenters/course_show_presenter.rb
+++ app/presenters/course_show_presenter.rb
@@ -1,20 +1,24 @@
 class CourseShowPresenter < Sufia::WorkShowPresenter
   def lecture_files
     lecture_files = []
-    query = ActiveFedora::SolrQueryBuilder.construct_query_for_ids(ordered_ids - file_set_ids)
-    results = ActiveFedora::SolrService.instance.conn.get(
+    query = ActiveFedora::SolrQueryBuilder.construct_query_for_ids(ordered_work_ids)
+    lectures = ActiveFedora::SolrService.instance.conn.get(
         ActiveFedora::SolrService.select_path,
-        params: { fq: query})
-    results["response"]["docs"].each do |member|
-      i=1
-      member_solr = SolrDocument.new(member)
-      member_solr.file_set_ids.each do |file_set_id|
-        track_title = member_solr.title.first
-        track_title += " i" if member_solr.file_set_ids.count > 1
+        params: { fq: query})["response"]["docs"].map { |res| SolrDocument.new(res) }
+    ordered_work_ids.each do |lecture_id|
+      lecture = lectures.find { |lecture| lecture.id == lecture_id }
+      i=1 #index for lectures with multiple audio files
+      lecture.file_set_ids.each do |file_set_id|
+        track_title = lecture.title.first
+        track_title += " i" if lecture.file_set_ids.count > 1
         lecture_files += [{id: file_set_id, title: track_title}]
       end
     end
     lecture_files
+  end
+
+  def ordered_work_ids
+    @ordered_work_ids ||= ordered_ids - file_set_ids
   end
 
 
