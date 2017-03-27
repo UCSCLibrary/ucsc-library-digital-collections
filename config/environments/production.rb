29c2cc3238d39fd01af64e68e716f94353677465
Ned Henry <ned.henry@gmail.com>
2017-03-27 15:08:21 -0700

    try to fix tinymce imageupload deployment bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.tinymce.install = :copy
+  config.tinymce.install = :compile
 
   # Code is not reloaded between requests.
   config.cache_classes = true
