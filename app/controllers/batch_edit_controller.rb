406cd20762520960f327b8eb362e5cc8f4e239e3
Ned Henry <ned.henry@gmail.com>
2017-03-28 14:25:53 -0700

    delete obsolete controllers and forms

--- app/controllers/batch_edit_controller.rb
+++
@@ -1,6 +0,0 @@
-class BatchEditsController < ApplicationController
-  include Hydra::BatchEditBehavior
-  include FileSetHelper
-  include Sufia::BatchEditsControllerBehavior
-  include Ucsc::BatchEditsControllerBehavior
-end
