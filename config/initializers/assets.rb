0facb48a4a5e4335c29c49cc0e7591c59b8ea9b4
Ned Henry <ned.henry@gmail.com>
2017-04-04 16:56:58 -0700

    fixed embed player error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
+Rails.application.config.assets.precompile += %w( include_player.js )
 Rails.application.config.assets.precompile += %w( embed.js )
 Rails.application.config.assets.precompile += %w( embed.css )
+
+
