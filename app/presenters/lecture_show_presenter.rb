b05b44d77c5991bac1637140e4c0cc47dc50e3fe
Ned Henry <ned.henry@gmail.com>
2017-05-18 08:35:36 -0700

    fix some lecture display issues

--- app/presenters/lecture_show_presenter.rb
+++ app/presenters/lecture_show_presenter.rb
@@ -4,27 +4,27 @@ class LectureShowPresenter < Sufia::WorkShowPresenter
 
   def creator
     return solr_document.creator unless solr_document.creator.nil?
-    parent_course.creator
+    parent_course.creator unless parent_course.nil?
   end
 
   def digital_publisher_homepage
     return solr_document.digital_publisher_homepage unless solr_document.digital_publisher_homepage.nil?
-    parent_course.digital_publisher_homepage
+    parent_course.digital_publisher_homepage unless parent_course.nil?
   end
 
   def language
     return solr_document.language unless solr_document.language.empty?
-    parent_course.language
+    parent_course.language unless parent_course.nil?
   end
 
   def physical_format
     return solr_document.physical_format unless solr_document.physical_format.nil?
-    parent_course.physical_format
+    parent_course.physical_format unless parent_course.nil?
   end
 
   def date_digitized
     return solr_document.date_digitized unless solr_document.date_digitized.nil?
-    parent_course.date_digitized
+    parent_course.date_digitized unless parent_course.nil?
   end
 
   def file_set_ids
