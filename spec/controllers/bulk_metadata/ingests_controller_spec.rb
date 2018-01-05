372d06970901a5858dc6c2a16ea1dd3231ade22f
Ned Henry <ethenry@ucsc.edu>
2018-01-04 20:32:13 -0800

    add template for bulk metadata controller spec (failing for now)

---
+++ spec/controllers/bulk_metadata/ingests_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe BulkMetadata::IngestsController do
+  include Devise::Test::ControllerHelpers
+
+  routes { Rails.application.routes }
+  let(:main_app) { Rails.application.routes.url_helpers }
+  let(:user) { create(:user) }
+  let(:work) { create(:work) }
+
+  before do 
+    sign_in user 
+  end
+
+  describe '#new' do
+    context 'ingest request' do
+      it 'shows me the ingest creation page' do
+        get :new
+#        expect(response).to be_success
+#        expect(response).to render_template("layouts/dashboard")
+      end
+    end
+  end
+
+
+  describe '#show' do
+    context 'an existing ingest' do
+      it 'shows me the ingest' do
+        get :show, params: {id: work.id}
+#        expect(response).to be_success
+#        expect(response).to render_template("layouts/dashboard")
+      end
+    end
+  end
+
+
+
+end
