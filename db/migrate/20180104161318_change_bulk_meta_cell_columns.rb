4e5ff556eaeab3486ec364f00207d351908c95a4
Ned Henry <ethenry@ucsc.edu>
2018-02-13 13:44:51 -0800

    Hacked up missing database migration to rename bmi tables

--- db/migrate/20180104161318_change_bulk_meta_cell_columns.rb
+++ db/migrate/20180104161318_change_bulk_meta_cell_columns.rb
@@ -1,5 +1,11 @@
 class ChangeBulkMetaCellColumns < ActiveRecord::Migration[5.0]
   def change
+    rename_table :bmi_cells, :bulk_meta_cells
+    rename_table :bmi_rows, :bulk_meta_rows
+    rename_table :bmi_ingests, :bulk_meta_ingests
+    rename_table :bmi_logs, :bulk_meta_logs
+    rename_table :bmi_relationships, :bulk_meta_relationships
+
     change_table :bulk_meta_cells do |t|
       t.rename :value_string, :value
     end
