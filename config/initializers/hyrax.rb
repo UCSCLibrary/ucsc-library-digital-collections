adaff53782660ebf38b1dd89ab177a31550b538b
Ned Henry <ethenry@ucsc.edu>
2018-02-23 17:06:35 -0800

    remove share your work button from homepage

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -147,7 +147,7 @@ Hyrax.config do |config|
 
 
   # Should a button with "Share my work" show on the front page to all users (even those not logged in)?
-#  config.display_share_button_when_not_logged_in = false
+  config.display_share_button_when_not_logged_in = false
 
   # The user who runs batch jobs. Update this if you aren't using emails
   # config.batch_user_key = 'batchuser@example.com'
