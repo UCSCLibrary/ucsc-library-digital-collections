96052918c41cdfd8cd69ebdb955c7b1ded2997fc
Ned Henry <ethenry@ucsc.edu>
2018-02-28 13:47:13 -0800

    fix issue with parent courses in breadcrumbs

--- lib/ucsc/breadcrumbs_for_works.rb
+++ lib/ucsc/breadcrumbs_for_works.rb
@@ -4,22 +4,25 @@ module Ucsc
 
     def default_trail 
 
+      hide_breadcrumbs = true;
+      
       unless (col_id = presenter.member_of_collection_ids.first).nil?
         col = ::SolrDocument.find(col_id)
         add_breadcrumb col.title.first, "/records/#{col_id}"
+        hide_breadcrumbs = false;
       end
 
-      unless (crs_id = presenter.solr_document.parent_course).nil?
-        crs = ::SolrDocument.find(crs_id)
+      unless (crs = presenter.solr_document.parent_course).nil?
         add_breadcrumb col.title, "/records/#{crs_id}"
+        hide_breadcrumbs = false;
       end
 
-      unless (wrk_id = presenter.solr_document.parent_work).nil?
-        wrk = ::SolrDocument.find(wrk_id)
+      unless (wrk = presenter.solr_document.parent_work).nil?
         add_breadcrumb wrk.title, "/records/#{wrk_id}"
+        hide_breadcrumbs = false;
       end
 
-      add_breadcrumb presenter.to_s, main_app.polymorphic_path(presenter)
+      add_breadcrumb presenter.to_s, main_app.polymorphic_path(presenter) unless hide_breadcrumbs
 
     end
 
