91925994aa8808e1d2574212052a7876ab2b8e5c
Ned Henry <ethenry@ucsc.edu>
2018-04-16 17:30:37 -0700

    fix Hyrax's persistent url function

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -61,7 +61,7 @@ Hyrax.config do |config|
   # config.temp_file_base = '/home/developer1'
 
   # Hostpath to be used in Endnote exports
-  # config.persistent_hostpath = 'http://localhost/files/'
+  config.persistent_hostpath = 'http://localhost/records/'
 
   # If you have ffmpeg installed and want to transcode audio and video set to true
    config.enable_ffmpeg = true
