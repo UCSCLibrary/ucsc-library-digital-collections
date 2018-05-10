bf0fc1fbc9df768ce38a4e4445f42f9f2da52e23
NedHenry <ethenry@ucsc.edu>
2018-05-10 08:04:35 -0700

    implement autogenerate local vocab

--- app/models/bulk_metadata/row.rb
+++ app/models/bulk_metadata/row.rb
@@ -1,8 +1,11 @@
+require 'uri'
+require 'socket'
+
 class BulkMetadata::Row < ApplicationRecord
 
   self.table_name = "bulk_meta_rows"
 
-  BASE_PATH = "/avalon2sufia/inbox"
+  BASE_PATH = "/dams_ingest"
   belongs_to :ingest 
   has_many :cells
   has_many :relationships
@@ -11,15 +14,10 @@ class BulkMetadata::Row < ApplicationRecord
     ScoobySnacks::METADATA_SCHEMA["work_types"][ingest.work_type.downcase]
   end
 
-  def parse
-    #TODO
-    #this method only applies if the text of the row is edited after failure
-    #parse self.text
-    #make sure it matches up with headers array
-    #(get array headers from parent csv file if necessary)
-    #update status
-    #create log
-    #return parsed array
+  def reparse
+    row_hash = CSV.parse(ingest.get_csv([id]),headers:true).first
+    cells.each{ |cell| cell.destroy! }
+    createNewCells!(row_hash)
   end
 
   def updateText(text)
@@ -29,16 +27,15 @@ class BulkMetadata::Row < ApplicationRecord
   end
 
   def createNewCells!(row_hash)
-    puts "work_type: #{ingest.work_type}"
-    puts "schema: #{schema.to_s}"
     row_hash.each do |property,values|
       next if values.nil?
       row_errors = []
       values.split(';').each do |value|
 
         if schema["properties"][property] && schema["properties"][property]["controlled"]
-          cell = cells.find_by(name: property, value_url: value )
-          cell ||= self.cells.create(name: property, value_url: value, value: value, status: "pending")
+          url = (value =~ URI::regexp) ? value : localAuthUrl(property, value)
+          cell = cells.find_by(name: property, value_url: url )
+          cell ||= self.cells.create(name: property, value_url: url, value: value, status: "pending")
         else
           cell = cells.find_by(name: property, value: value )
           cell ||= self.cells.create(name: property, value: value, status: "pending") 
@@ -54,6 +51,51 @@ class BulkMetadata::Row < ApplicationRecord
   #edit identifier
   #ignore
 
+  def localAuthUrl(property, value) 
+    return value if (auth = getLocalAuth(property)).nil?
+    return (url = findAuthUrl(auth, value)) ? url : mintLocalAuthUrl(auth,value)
+  end
+
+  def mintLocalAuthUrl(auth_name, value) 
+    id = value.parameterize
+    auth = Qa::LocalAuthority.find_or_create_by(name: auth_name)
+    Qa::LocalAuthorityEntry.create(local_authority: auth,
+                                   label: value,
+                                   uri: id)
+    return localIdToUrl(id,auth)
+  end
+
+  def findAuthUrl(auth, value)
+    return nil if auth.nil?
+    return nil unless (entries = Qa::Authorities::Local.subauthority_for(auth).search(value))
+    entries.each do |entry|
+      #require exact match
+      if entry["label"] == value
+        url = entry["url"]
+        url ||= entry["id"]
+        url = localIdToUrl(url,auth) unless url =~ URI::regexp
+        return url
+      end
+    end
+    return nil
+  end
+
+  def localIdToUrl(id,auth) 
+    return "https://digitalcollections.library.ucsc.edu/authorities/show/local/#{auth}/#{id}"
+  end
+
+  def getLocalAuth(property)
+     if vocs = schema["properties"][property]["vocabularies"]
+       vocs.each do |voc|
+         return voc["subauthority"] if voc["authority"].downcase == "local"
+       end
+     elsif voc = schema["properties"][property]["vocabulary"]
+       return voc["subauthority"] if voc["authority"].downcase == "local"
+     end
+     return nil
+  end
+
+    
   def ingest!(user_email)
     user = User.find_by_email(user_email)
     metadata = {}
@@ -77,8 +119,8 @@ class BulkMetadata::Row < ApplicationRecord
           file = File.open(File.join(BASE_PATH,cell.value))
           uploaded_file = Hyrax::UploadedFile.create(file: file, user: user)
           (metadata[:uploaded_files] ||= []) << uploaded_file.id if !uploaded_file.id.nil?
-        rescue
-          self.status = "Error: Cannot open file"
+        rescue Exception => e  
+          self.status = "Error: Cannot open file: #{File.join(BASE_PATH,cell.value)}, #{e.message}"
           self.save
           return false
         end
@@ -158,9 +200,9 @@ class BulkMetadata::Row < ApplicationRecord
 
         next unless schema["properties"][property_name]
 
-
         if schema["properties"][property_name]["controlled"]
           value = cell.value_url ? cell.value_url : cell.value
+          
           metadata["#{property_name}_attributes"] ||= []
           metadata["#{property_name}_attributes"] << {id: value.strip}
         else
