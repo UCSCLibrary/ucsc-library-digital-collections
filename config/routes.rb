791283495a416f1d692b69e761c601628140784e
NedHenry <ethenry@ucsc.edu>
2018-06-06 11:50:09 -0700

    add route for bulk workflow action

--- config/routes.rb
+++ config/routes.rb
@@ -81,5 +81,7 @@ Rails.application.routes.draw do
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  put '/dashboard/workflow_actions/update(.:format)', to: 'ucsc/workflow_actions#update'
+  patch '/dashboard/workflow_actions/update(.:format)', to: 'ucsc/workflow_actions#update'
+  
 end
