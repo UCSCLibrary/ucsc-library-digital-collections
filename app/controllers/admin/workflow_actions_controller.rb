4eae4b39d92ed057cccdafcfe148daa919acb89d
NedHenry <ethenry@ucsc.edu>
2018-06-12 12:39:47 -0700

    fix accidental references to undefined variables

--- app/controllers/admin/workflow_actions_controller.rb
+++ app/controllers/admin/workflow_actions_controller.rb
@@ -5,7 +5,7 @@ class Admin::WorkflowActionsController < Hyrax::WorkflowActionsController
     errors = []
     action = Sipity::WorkflowAction.where(name: params["workflow_action"], workflow_id: params["workflow_id"]).first!
     params['ids'].each do |id|
-      subject = Hyrax::WorkflowActionInfo.new(work, user)
+      subject = Hyrax::WorkflowActionInfo.new(ActiveFedora::Base.find(id), current_user)
       Hyrax::Workflow::WorkflowActionService.run(subject: subject, action: action)
     end
      redirect_to '/admin/workflows', notice: "The states of your works have been updated."
