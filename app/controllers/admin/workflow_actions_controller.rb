c9ff3fcf9323033176c98e5e27d99ccb039079e6
NedHenry <ethenry@ucsc.edu>
2018-06-12 10:33:35 -0700

    clean up superfluous code

--- app/controllers/admin/workflow_actions_controller.rb
+++ app/controllers/admin/workflow_actions_controller.rb
@@ -8,14 +8,7 @@ class Admin::WorkflowActionsController < Hyrax::WorkflowActionsController
       subject = Hyrax::WorkflowActionInfo.new(work, user)
       Hyrax::Workflow::WorkflowActionService.run(subject: subject, action: action)
     end
-
      redirect_to '/admin/workflows', notice: "The states of your works have been updated."
- 
-  end
-  
-  def workflow_action_params
-    #      @workflow_action_params ||= params.require(:workflow_action).permit(:name, :comment)
-    params
   end
 
 end
