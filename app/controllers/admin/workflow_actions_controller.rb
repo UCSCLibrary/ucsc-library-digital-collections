f2c4c912c8f4f80537e82678e5803b290ff64a3f
NedHenry <ethenry@ucsc.edu>
2018-06-12 10:21:34 -0700

    fix code to apply workflow state changes

--- app/controllers/admin/workflow_actions_controller.rb
+++ app/controllers/admin/workflow_actions_controller.rb
@@ -3,25 +3,14 @@ class Admin::WorkflowActionsController < Hyrax::WorkflowActionsController
   def update
     return super if params['ids'].blank?
     errors = []
-#    action = Sipity::WorkflowAction.where(name: params["workflow_action"], workflow_id: params["workflow_id"]).first
+    action = Sipity::WorkflowAction.where(name: params["workflow_action"], workflow_id: params["workflow_id"]).first!
     params['ids'].each do |id|
-      form = Hyrax::Forms::WorkflowActionForm.new(
-        current_ability: current_ability,
-        work: ActiveFedora::Base.find(id),
-        attributes: {name: params["workflow_action"]})
-      unless form.save
-        errors << id
-      end
+      subject = Hyrax::WorkflowActionInfo.new(work, user)
+      Hyrax::Workflow::WorkflowActionService.run(subject: subject, action: action)
     end
 
-    #      if errors.blank?
-    redirect_to '/admin/workflows', notice: "The states of your works have been updated. id: #{errors.first.to_s}    actionid:  #{action.id}"
-    #      else
-    #        Rails.logger.error("error updating works: #{errors.join(', ')}")
-    #        puts "error updating works: #{errors.join(', ')}"
-    #        render 'hyrax/base/unauthorized', status: :unauthorized
-    #      end
-
+     redirect_to '/admin/workflows', notice: "The states of your works have been updated."
+ 
   end
   
   def workflow_action_params
