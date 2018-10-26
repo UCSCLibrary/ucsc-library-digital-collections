9534ded8615f77b167df28037a813a364cdea387
NedHenry <ethenry@ucsc.edu>
2018-10-26 16:39:20 -0700

    add missing comma in migration file

--- db/migrate/20181026232205_rename_github_credentials_table.rb
+++ db/migrate/20181026232205_rename_github_credentials_table.rb
@@ -1,5 +1,5 @@
 class RenameGithubCredentialsTable < ActiveRecord::Migration[5.0]
   def change
-    rename_table :github_credentials :bulk_ops_github_credentails
+    rename_table :github_credentials, :bulk_ops_github_credentails
   end
 end
