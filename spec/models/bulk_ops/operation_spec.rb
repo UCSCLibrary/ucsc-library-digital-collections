c82f8f1cfb8509ca3acda98afbeb4535a490cc18
Ned Henry <ethenry@ucsc.edu>
2018-10-31 12:35:40 -0700

    added test coverage for bulk operations github related functions

---
+++ spec/models/bulk_ops/operation_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+RSpec.describe BulkOps::Operation do
+  
+  describe "A bulk update" do
+    let(:usr) {User.create(email:"test user email")}
+    let!(:draft) {described_class.create(name: "rspec test branch", user_id: usr.id, stage: "draft", operation_type: "update", status: "new")}
+    let!(:wrk1) {Work.create(depositor: usr.email, title:["test title"])}
+    let!(:wrk2) {Work.create(depositor: usr.email, title:["test title"])}
+    let!(:proxy1) {draft.work_proxys.create(work_id: wrk1.id,status:"new")}
+    let!(:proxy2) {draft.work_proxys.create(work_id: wrk2.id,status:"new")}
+
+    before(:each)  do 
+#      @usr = User.create(email:"test user email")
+#      @draft = described_class.create(name: "rspec test branch", user_id: @usr.id, stage: "draft", operation_type: "update", status: "new")
+#      wrk1 = Work.create(depositor: @usr.email, title:["test title"])
+#      wrk2 = Work.create(depositor: @usr.email, title:["test title"])
+#      @draft.work_proxys.create(work_id: wrk1.id,status:"new")
+#      @draft.work_proxys.create(work_id: wrk2.id,status:"new")    
+      draft.create_branch
+    end
+    after(:each) {draft.delete_branch}
+
+    it "is persisted in the database" do
+      expect(described_class.find_by(id: draft.id)).to be_present
+    end
+    
+    it "can recognize its contained works" do
+      expect(draft.work_proxys.count).to eq(2)
+    end
+
+    it "puts a template options file in its new github branch" do
+      expect(BulkOps::GithubAccess.load_options(draft.name)).to be_instance_of(Hash)
+    end
+    
+    it "puts a spreadsheet of existing data in its new github branch" do
+      sheet = BulkOps::GithubAccess.load_metadata(draft.name)
+      expect(sheet).to be_instance_of(CSV::Table)
+      expect(sheet.count).to eq(draft.work_proxys.count)
+    end
+
+    it "can update the operation status appropriately" do
+      draft.stage = "pending"
+      draft.status = "ok"
+      draft.message = "Update branch created in github"
+      draft.save
+      expect(described_class.find(draft.id).stage).to eq("pending")
+    end
+
+    context "with an updated sheet" do
+      let(:tmp_file){Tempfile.new('test-update-sheet')}
+      let(:altered_title){"new altered test title"}
+
+      before(:each) do
+        sheet = BulkOps::GithubAccess.load_metadata(draft.name)
+        sheet.first["title"] = altered_title
+        CSV.open(tmp_file, "wb") do |csv|
+          csv << sheet.headers
+          sheet.each {|row| csv << row.fields}
+        end
+        draft.update_spreadsheet tmp_file.path
+      end
+
+      after(:each){tmp_file.unlink}
+
+      it "succesfully updates the spreadsheet in Github" do
+        updated_sheet = BulkOps::GithubAccess.load_metadata(draft.name)
+        expect(updated_sheet.first["title"]).to eq(altered_title)
+      end
+    end
+
+    context "with updated options" do
+      let(:altered_status){"new altered status"}
+
+      before(:each) do
+        options = BulkOps::GithubAccess.load_options(draft.name)
+        options["status"] = altered_status
+        draft.update_options options
+      end
+
+      it "succesfully updates the config file in Github" do
+        updated_options = BulkOps::GithubAccess.load_options(draft.name)
+        expect(updated_options["status"]).to eq(altered_status)
+      end
+    end
+
+  end
+end
---
+++ spec/services/bulk_ops/github_access_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe BulkOps::GithubAccess do
+  let!(:usr) { create(:user) }
+  let!(:git) {described_class.new("rspec test branch")}
+
+  before(:each) do
+    git.create_branch!
+  end
+
+  after(:each) do
+    git.delete_branch!
+  end
+
+  describe "branch management" do
+    let!(:branches) {git.list_branches}
+    let!(:branch_names) {git.list_branch_names}
+
+
+    it "can return a list of branches in the repo" do
+      expect(branches).to be_instance_of(Array)
+      expect(branch_names).to be_instance_of(Array)
+    end
+
+    it "does not include the master branch in the branch list" do
+      expect(branch_names).not_to include("master")
+    end
+
+    it "finds our newly created branch" do
+      expect(branch_names).to include(git.name)
+    end
+
+    it "finds some options for our new branch" do
+      git.load_options.to be_instance_of(Hash)
+    end
+
+    it "can delete this new branch" do
+      git.delete_branch!
+      sleep(0.5)
+      expect(git.list_branch_names).not_to include(git.name)
+    end
+
+  end
+
+  describe "file management" do
+
+    context "with a new file added from the filesystem" do 
+      it "can retrieve and verify the file's contents" do
+
+      end
+    end
+
+
+    context "With new file by its contents" do
+      it "can retrieve and verify the file's contents" do
+
+      end
+    end
+
+    context "With an update to the " do
+      it "can retrieve and verify the file's contents" do
+
+      end
+    end
+
+    it "can auto-update the 'options' file for this bulk operation" do
+
+    end
+
+    it "can update the spreadsheet for this bulk operation" do
+
+    end
+
+    
+
+
+  end
+
+end
