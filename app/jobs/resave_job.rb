6ff27b979df6bd77430a116a184792e9ce68bc75
NedHenry <ethenry@ucsc.edu>
2019-06-03 09:49:59 -0700

    fix merging of overlapping changes from two dev machines/branches

---
+++ app/jobs/resave_job.rb
@@ -0,0 +1,18 @@
+class ResaveJob < Hyrax::ApplicationJob
+  def perform(work_types=nil)
+    rows = 100
+    work_types ||= ["Work","Course","Lecture"]
+    work_types.each do |work_type|
+      query = ActiveFedora::SolrQueryBuilder.construct_query_for_rel("has_model" => work_type)
+      num_works = ActiveFedora::SolrService.instance.conn.get(ActiveFedora::SolrService.select_path, params: { fq: query, rows: 0})["response"]["numFound"]    
+      start = 0
+      until start > num_works
+        works = ActiveFedora::SolrService.instance.conn.get(ActiveFedora::SolrService.select_path, params: { fq: query, rows: rows, start: start})["response"]["docs"]
+        works.each do  |wrk|
+          ActiveFedora::Base.find(wrk['id']).save
+        end
+        start = start + rows
+      end
+    end
+  end
+end
