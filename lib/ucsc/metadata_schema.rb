7e471e30569a614320e9a0ce5c6a7b5a435a0d2b
NedHenry <ethenry@ucsc.edu>
2019-06-03 17:58:21 -0700

    use ScoobySnacks method to define display group methods (fixing bug)

--- lib/ucsc/metadata_schema.rb
+++ lib/ucsc/metadata_schema.rb
@@ -1,24 +1,19 @@
 module Ucsc
   class MetadataSchema < ScoobySnacks::MetadataSchema
 
-    def custom_display_groups
+    def self.custom_display_groups
       [:admin_only]
     end
+
+     define_display_group_methods(custom_display_groups)
 
     def display_field_names
       super + admin_only_display_field_names
     end
 
-    def admin_only_display_fields
-      @admin_only_display_field_names.map{|name| get_field(name)} unless @admin_only_display_field_names.blank?
-      fields = @fields.values.select{|field| field.in_display_group? "admin" && !field.hidden?}
-      @admin_only_display_field_names = fields.map{|field| field.name}
-      return fields
+    def display_fields
+      super + admin_only_display_fields
     end
-
-    def admin_only_display_field_names
-      @admin_only_display_field_names || admin_only_display_fields.map{|field| field.name}
-    end
-
+    
   end
 end
