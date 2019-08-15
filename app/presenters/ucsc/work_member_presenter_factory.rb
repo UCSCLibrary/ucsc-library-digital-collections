0d82fb0de36b7ad9d5056dea0c194faa4bf44912
NedHenry <ethenry@ucsc.edu>
2019-08-15 09:35:03 -0700

    alter #file_set_presenters (which determines what goes in IIIF manifest) to include grandchild media (e.g. images attached to child works)

--- app/presenters/ucsc/work_member_presenter_factory.rb
+++ app/presenters/ucsc/work_member_presenter_factory.rb
@@ -3,12 +3,18 @@ module Ucsc
     
     self.work_presenter_class = WorkShowPresenter
 
+    delegate :grandchild_file_set_ids, to: :@work
+
     def member_av_files
       @member_av_files ||= generate_member_av_file_list()
     end
 
     def ordered_work_ids
-      @ordered_work_ids ||= ordered_ids - file_set_ids
+      @ordered_work_ids ||= (ordered_ids - file_set_ids)
+    end
+
+    def file_set_presenters
+      @file_set_presenters ||= member_presenters((ordered_ids & file_set_ids) + grandchild_file_set_ids)
     end
 
     private 
