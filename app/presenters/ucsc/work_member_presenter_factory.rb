9f5749204952ceabd60d793119bb64c057ca6348
NedHenry <ethenry@ucsc.edu>
2019-09-03 13:36:31 -0700

    do not display duplicate member works

--- app/presenters/ucsc/work_member_presenter_factory.rb
+++ app/presenters/ucsc/work_member_presenter_factory.rb
@@ -14,7 +14,7 @@ module Ucsc
     end
 
     def file_set_presenters
-      @file_set_presenters ||= member_presenters((ordered_ids & file_set_ids) + grandchild_file_set_ids)
+      @file_set_presenters ||= member_presenters(((ordered_ids & file_set_ids) + grandchild_file_set_ids).uniq)
     end
 
     private 
