18fda5ebf324acea6d15cc1eab31e7c88c566c2f
NedHenry <ethenry@ucsc.edu>
2019-10-11 11:25:13 -0700

    update obsolete solr_document tests to reflect current desired functionality

--- spec/models/solr_document_spec.rb
+++ spec/models/solr_document_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe SolrDocument do
 
-  describe "the solr_name class method" do
+  describe "solr_name class method" do
     it "properly returns solr names based on indexing parameters" do
       expect(described_class.solr_name("title")).to include("title_tesim")
 #      expect(described_class.solr_name("keyword")).to include("keyword_ssim")
@@ -14,17 +14,18 @@ RSpec.describe SolrDocument do
       expect(described_class.instance_methods.grep(/physicalDescription/).count).to eq(1)
       expect(described_class.instance_methods.grep(/language/).count).to eq(1)
       expect(described_class.instance_methods.grep(/keyword/).count).to eq(1)
-      expect(described_class.instance_methods.grep(/staffNote/).count).to eq(0)
+      expect(described_class.instance_methods.grep(/staffNote/).count).to eq(1)
     end
   end
 
-  describe "the field_semantics class method" do
+  describe "field_semantics class method" do
     it("returns customized oai feed parameters") do
-      expect(described_class.field_semantics.keys).to include("rights","language")
-      expect(described_class.field_semantics["creator"]).to include("creator_tesim")
-      expect(described_class.field_semantics["type"]).to include("resourceType_tesim")
+      expect(described_class.field_semantics.keys).to include("DC:type","DPLA:isShownAt")
+      expect(described_class.field_semantics["DC:creator"]).to include("creator")
+      expect(described_class.field_semantics["DC:type"]).to include("resourceType")
       expect(described_class.field_semantics.keys).not_to include(/physicalDescription/)
       expect(described_class.field_semantics.values).not_to include(/physicalDescription/)
     end
   end
+
 end
