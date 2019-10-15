50725504cd9744a9e906f32f330ab3824e7613ba
NedHenry <ethenry@ucsc.edu>
2019-10-15 16:05:38 -0700

    refactor and simplify to save solr calls

--- app/presenters/ucsc/work_member_presenter_factory.rb
+++ app/presenters/ucsc/work_member_presenter_factory.rb
@@ -25,17 +25,12 @@ module Ucsc
 
     def generate_member_av_file_list
       member_av_files = []
-      query = ActiveFedora::SolrQueryBuilder.construct_query_for_ids(ordered_work_ids)
-      works = ActiveFedora::SolrService.instance.conn.get(
-        ActiveFedora::SolrService.select_path,
-        params: { fq: query, rows: 1000})["response"]["docs"].map { |res| SolrDocument.new(res) }
       ordered_work_ids.each do |work_id|
-        work = works.find { |work| work.id == work_id }
+        work = SolrDocument.find(work_id)
         next if work.nil?
         i=1 #index for works with multiple audio files
-        work.file_set_ids.each do |file_set_id|
-          fs = SolrDocument.find(file_set_id)
-          next unless fs.audio? || fs.video?
+        solr_docs = work.file_set_ids.map {|file_set_id| SolrDocument.find(file_set_id)}.select{fs.audio? || fs.video?}
+        solr_docs.map do |doc|
           track_title = work.title.first
           track_title += " #{i}" if work.file_set_ids.count > 1
           member_av_files += [{id: file_set_id, title: track_title}]
