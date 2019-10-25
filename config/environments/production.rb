9ff579af4c095b0dbc352c8af15f9d6cb5638056
NedHenry <ethenry@ucsc.edu>
2019-10-25 14:47:08 -0700

    use a filesystem cache store in production (for now - move to Redis store in the future)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,17 +57,15 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+   config.cache_store = :file_store, "/dams_derivatives/production/rails-cache"
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :sidekiq
   # config.active_job.queue_name_prefix = "ucsc_sufia_#{Rails.env}"
 
-
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { :host => 'digitalcollections.library.ucsc.edu' }
-
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
