a7f0fb8144923d52b89dfc069d707ade6a66df33
NedHenry <ethenry@ucsc.edu>
2019-10-25 11:09:36 -0700

    overwrite manifest action to cache work manifests

--- app/controllers/hyrax/works_controller.rb
+++ app/controllers/hyrax/works_controller.rb
@@ -7,5 +7,23 @@ module Hyrax
 
     self.curation_concern_type = ::Work
     self.show_presenter = Ucsc::WorkShowPresenter
+
+    def manifest
+      headers['Access-Control-Allow-Origin'] = '*'
+      @cache_key = "manifest/#{presenter.id}"
+      respond_to do |wants|
+        wants.json { render json: cached_manifest }
+        wants.html { render json: cached_manifest }
+      end
+    end
+
+    def cached_manifest
+      modified = presenter.solr_document.modified || DateTime.now
+      @cache_key = "manifest/#{presenter.id}"
+      if (entry = Rails.cache.send(:read_entry,@cache_key,{})).present?
+        Rails.cache.delete(@cache_key) if (Time.at(entry.instance_variable_get(:@created_at)) < presenter.solr_document.modified)
+      end
+      Rails.cache.fetch(@cache_key){ manifest_builder.to_h.to_json}
+    end
   end
 end
