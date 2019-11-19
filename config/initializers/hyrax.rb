b97af292f6d53237481673f01e3408083fcd1b81
NedHenry <ethenry@ucsc.edu>
2019-11-19 15:23:21 -0800

    generate IIIF urls using a new format to make it easier for cantaloupe to access j2k files directly

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -135,13 +135,28 @@ Hyrax.config do |config|
    config.iiif_image_server = true
 
    # If we have an external IIIF server, use it for image requests; else, use riiif
-   config.iiif_image_url_builder = lambda do |file_id, base_url, size|
-     unless (image_server_root_url = ENV['IIIF_SERVER_URL'])
-       port = (Rails.env.to_s == "production") ? 8183 : 8185
-       image_server_root_url = "https://digitalcollections-image.library.ucsc.edu:#{port}/iiif/2/"
+   config.iiif_image_url_builder = lambda do |fileset_id, base_url, size|
+
+     if fileset_id.split("files").count > 1
+       # When we were taking images directly from Fedora, we would pass 
+       # "#{file_set.id}/files/#{file_set.original_file.id}" 
+       # as a fileset_id to locate the original file binary in Fedora
+       # Here we include a hack to take the fileset.id from the old syntax
+       fid = fileset_id[0..8]
+     else
+       fid = fileset_id
      end
+
+     # Divide the file id with underscores for the image server to handle more easily
+     encoded_id = "#{fid[0..1]}_#{fid[2..3]}_#{fid[4..5]}_#{fid[6..7]}_#{fid[8]}"
+
+     unless (image_server_root_url = ENV['IIIF_SERVER_URL'])
+       suffix = (Rails.env.to_s == "production") ? "" : "staging/"
+       image_server_root_url = "https://digitalcollections-image.library.ucsc.edu/#{suffix}iiif/2/"
+     end
+
      iiif_url = File.join(image_server_root_url,
-                          file_id.gsub('/', '%2F'),
+                          encoded_id,
                           "full",
                           size,
                           "0",
@@ -151,12 +166,18 @@ Hyrax.config do |config|
    end
 
    # If we have an external IIIF server, use it for info.json; else, use riiif
-   config.iiif_info_url_builder = lambda do |file_id, base_url|
-     unless (image_server_root_url = ENV['IIIF_SERVER_URL'])
-       port = (Rails.env.to_s == "production") ? 8183 : 8185
-       image_server_root_url = "https://digitalcollections-image.library.ucsc.edu:#{port}/iiif/2/"
+   config.iiif_info_url_builder = lambda do |fileset_id, base_url|
+     if fileset_id.split("files").count > 1
+       fileset_id = fileset_id[0..8]
      end
-     File.join(image_server_root_url, file_id.gsub('/', '%2F'))
+     fid=fileset_id
+     unless (image_server_root_url = ENV['IIIF_SERVER_URL'])
+       suffix = (Rails.env.to_s == "production") ? "" : "staging/"
+       image_server_root_url = "https://digitalcollections-image.library.ucsc.edu/#{suffix}iiif/2/"
+     end
+     # Divide the file id with underscores for the image server to handle more easily
+     encoded_id = "#{fid[0..1]}_#{fid[2..3]}_#{fid[4..5]}_#{fid[6..7]}_#{fid[8]}"
+     File.join(image_server_root_url,encoded_id)
    end
 
   # Returns a URL that indicates your IIIF image server compliance level
