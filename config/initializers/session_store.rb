2da0d37f97db5dca85eab44c2793290ad9265dc8
NedHenry <ethenry@ucsc.edu>
2020-01-06 14:16:44 -0800

    expose cookies to a slightly wider domain, so image server can read them

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ucsc_hyrax_session'
+domain = ["production", "staging"].include?(Rails.env) ? '.library.ucsc.edu' : :all
+Rails.application.config.session_store :cookie_store, key: '_ucsc_hyrax_session', domain: domain
