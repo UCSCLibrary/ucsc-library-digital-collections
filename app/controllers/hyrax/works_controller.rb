fee8bb3fb632e86a1afc616bb03baa8eba3c1d3e
NedHenry <ethenry@ucsc.edu>
2020-02-24 17:21:37 -0800

    add endpoint to zip citation with image file and stream to user

--- app/controllers/hyrax/works_controller.rb
+++ app/controllers/hyrax/works_controller.rb
@@ -8,6 +8,27 @@ module Hyrax
     self.curation_concern_type = ::Work
     self.show_presenter = Ucsc::WorkShowPresenter
 
+
+    def zip_media_citation
+      #This feature is currently only implemented for Images
+      size = params['size'] || "1000,"
+      image_url = Hyrax.config.iiif_image_url_builder.call(presenter.representative_id, nil,size).gsub("localhost","images")
+      zip_io = Zip::OutputStream.write_buffer do |zio|
+        zio.put_next_entry("image_#{presenter.id}.jpg")
+        zio.write(open(image_url).read)
+        zio.put_next_entry("citation_#{presenter.id}.txt")
+        zio.puts "Citations for #{presenter.title.first}\n"
+        zio.puts "APA:"
+        zio.puts ActionView::Base.full_sanitizer.sanitize(Hyrax::CitationsBehaviors::Formatters::ApaFormatter.new(self).format(presenter)).strip+"\n"
+        zio.puts "MLA:"
+        zio.puts ActionView::Base.full_sanitizer.sanitize(Hyrax::CitationsBehaviors::Formatters::MlaFormatter.new(self).format(presenter)).strip+"\n"
+        zio.puts "Chicago"
+        zio.puts ActionView::Base.full_sanitizer.sanitize(Hyrax::CitationsBehaviors::Formatters::ChicagoFormatter.new(self).format(presenter)).strip+"\n"
+      end
+      zip_io.rewind
+      send_data zip_io.read, :filename => "media_citation_#{presenter.id}.zip"
+    end
+
     def manifest
       headers['Access-Control-Allow-Origin'] = '*'
       @cache_key = "manifest/#{presenter.id}"
