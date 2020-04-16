0dfbe6109eee25de08468d846ce885af346d4848
Ned Henry <ethenry@ucsc.edu>
2020-04-16 11:57:24 -0700

    include credentials for sauce to access staging environments, if necessary

--- spec/spec_helper_derailed.rb
+++ spec/spec_helper_derailed.rb
@@ -1,14 +1,25 @@
+require 'capybara'
 require 'capybara/rspec'
 require 'selenium/webdriver'
 
-#include Warden::Test::Helpers
+if ENV['SAUCE'] || ENV['SAUCE_USERNAME']
+  require 'sauce_spec_helper'
+else
+  #include Warden::Test::Helpers
+  Capybara.configure do |config|
+    config.default_driver = :selenium_headless
+  end
+end
+
 Capybara.configure do |config|
   config.run_server = false
-  config.default_driver = :selenium_headless
 
-  host_url = ENV["HOST_URL"] || "http://localhost:3000"
+  if (username = ENV['STAGING_USERNAME']).present? && (password = ENV['STAGING_PASSWORD']).present? && !Rails.env.production?
+    host_url = "https://#{username}:#{password}@#{Rails.config.hostname}"
+  else
+    host_url = "https://#{Rails.config.hostname}"
+  end
   config.app_host = host_url
-  
   host_parsed = URI.parse host_url
   config.server_host = host_parsed.host
   config.server_port = host_parsed.port
