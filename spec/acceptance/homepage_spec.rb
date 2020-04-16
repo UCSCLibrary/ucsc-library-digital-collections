da3cdd3970c438021c490f2611422d2c51c9e97e
Ned Henry <ethenry@ucsc.edu>
2020-04-16 11:44:03 -0700

    expand homepage tests

--- spec/acceptance/homepage_spec.rb
+++ spec/acceptance/homepage_spec.rb
@@ -1,11 +1,24 @@
 
 require 'spec_helper_derailed'
 
-RSpec.feature 'Homepage', js: false do
-  context 'a guest user' do
-    scenario do
-      visit '/'
-      expect(page).to have_content "UC Santa Cruz University Library Digital Media Collection"
-    end
+RSpec.feature 'A guest user visiting the site for the first time', js: false do
+  scenario 'loads the homepage' do
+    visit '/'
+    expect(page).to have_content "UC Santa Cruz University Library Digital Media Collection"
+    expect(page).to have_content "Accessibility Policy"
+    expect(page).to have_content "Takedown Policy"
+    expect(page).to have_content "Reproduction & Use"
+    expect(page).to have_content "Digital Collections Login"
+    expect(page).to have_content "About the UCSC Digital Collections"
+    expect(page).to have_content "UCSC Library GitHub"
+    expect(page).to have_content "Feedback"
+    expect(page).to have_selector '#feedbackModal', visible: false
+    expect(page).to have_content "Browse Collections"
+    expect(page).to have_content "Contact Us"
+    expect(page).to have_selector "input#search-field-header"
+    expect(page).to have_selector 'li.featured-item img'
+    click_on("Contact Us")
+    expect(page).to have_css("form")
+    expect(page).to have_content("FAQ")
   end
 end
