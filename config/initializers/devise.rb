3b936bdd31ff050fea589a758f6d9fc05f9783f2
Ned Henry <ethenry@ucsc.edu>
2020-06-09 10:19:40 -0700

    change from address in auto-generated emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@digitalcollections.ucsc.edu'
+  config.mailer_sender = 'noreply@digitalcollections.library.ucsc.edu'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
