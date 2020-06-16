94ec2a53d2f24b40b5d72af0b5e6e8ba1a7c61e3
Ned Henry <ethenry@ucsc.edu>
2020-06-16 16:02:57 -0700

    fix typo in file characterization patch

--- config/initializers/ch12n_patches.rb
+++ config/initializers/ch12n_patches.rb
@@ -2,7 +2,7 @@ module Hydra::Works
   class CharacterizationService
     def append_property_value (property, value)
       return if (property == :mime_type) && (value.to_s.include? "excel") && (object.mime_type.present?)
-      value = object.send(property) + [value] unless property == mime_type
+      value = object.send(property) + [value] unless property == :mime_type
       # We don't want multiple heights / widths, pick the max
       value = value.map(&:to_i).max.to_s if property == :height || property == :width
       object.send("#{property}=", value)
