ea3db2423f2c2ddd4ca88c333841d745a11fce69
Ryan Schwab <rwschwab@ucsc.edu>
2020-06-23 14:13:37 -0700

    Implement the about the collection section for works pages. Adds a new view partial and a collection method to the work show presenter.

--- app/presenters/ucsc/work_show_presenter.rb
+++ app/presenters/ucsc/work_show_presenter.rb
@@ -43,6 +43,11 @@ module Ucsc
       @all_av_files ||= generate_all_av_file_list
     end
 
+    def collection
+      return nil unless solr_document.member_of_collection_ids.present?
+      @collection ||= SolrDocument.find(solr_document.member_of_collection_ids.first)
+    end
+
     def parent
       return nil unless solr_document.parent_id.present?
       @parent ||= SolrDocument.find(solr_document.parent_id)
