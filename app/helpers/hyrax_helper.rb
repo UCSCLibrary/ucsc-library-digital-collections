7e34f8471bee4dfa612be01ce3ef16bbefc7f8ee
Ned Henry <ethenry@ucsc.edu>
2020-07-03 11:06:08 -0700

    fix custom visibility permission badges

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -2,4 +2,8 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+
+  def visibility_badge(value)
+    Ucsc::PermissionBadge.new(value).render
+  end
 end
--- app/presenters/ucsc/permission_badge.rb
+++ app/presenters/ucsc/permission_badge.rb
@@ -1,11 +1,6 @@
 module Ucsc
   class PermissionBadge < Hyrax::PermissionBadge
 
-    def dom_label_class
-      UCSC_VISIBILITY_LABEL_CLASS.fetch(@visibility.to_sym)
-    end
-
-
     UCSC_VISIBILITY_LABEL_CLASS = {
       authenticated: "label-info",
       embargo: "label-warning",
@@ -15,5 +10,9 @@ module Ucsc
       open: "label-success",
       restricted: "label-danger"
     }.freeze
+    
+    def dom_label_class
+      UCSC_VISIBILITY_LABEL_CLASS.fetch(@visibility.to_sym)
+    end
   end
 end
