f7733be3d3ef894a8c47e1bca0fb87a4eeec4ba4
NedHenry <ethenry@ucsc.edu>
2020-11-05 15:32:51 -0800

    improve job to publish whole collection in the background

--- app/jobs/publish_collection_job.rb
+++ app/jobs/publish_collection_job.rb
@@ -3,29 +3,36 @@ class PublishCollectionJob < Hyrax::ApplicationJob
     errors = []
     col = Collection.find(collection_id)
     rows = 100
-    query = ActiveFedora::SolrQueryBuilder.construct_query_for_rel("member_of_collection_ids" => collection_id)
+    query = ActiveFedora::SolrQueryBuilder.construct_query_for_rel("member_of_collection_ids" => collection_id, "visibility" => "restricted").gsub("visibility_ssim","visibility_ssi")
     num_works = ActiveFedora::SolrService.instance.conn.get(ActiveFedora::SolrService.select_path, params: { fq: query, rows: 0})["response"]["numFound"]
     until start > num_works
       docs = ActiveFedora::SolrService.instance.conn.get(ActiveFedora::SolrService.select_path, params: { fq: query, rows: rows, start: start})["response"]["docs"]
       docs.each do  |doc|
         begin
-          work = Work.find(doc['id'])
-          work.file_sets.each do |file_set|
-            file_set.visibility = "open"
-            file_set.save
-          end
-          work.visibility = "open"
-          work.save
+          publish_work doc["id"]
         rescue
-          errors << work.id
+          errors << doc['id']
         end
       end
       start = start + rows
-      Rails.logger.info "publish collection job finished batch #{start/rows} of #{num_rows/rows}"
+      Rails.logger.info "publish collection job finished batch #{start/rows} of #{num_works/rows}"
     end
-    col.visibility = "open"
-    col.save
+    publish(col)
     Rails.logger.warn "Found #{errors.count} errors during publication:"
     Rails.logger.warn errors.map(&:to_s).join(',')
+  end
+
+  private
+
+  def publish_work id
+    work = Work.find(id)
+    (work.member_ids - work.file_set_ids).each{|id| publish_work(id)}
+    work.file_sets.each{ |file_set| publish(file_set) }
+    publish(work)
+  end
+
+  def publish object
+    object.visibility = "open"
+    object.save
   end
 end
