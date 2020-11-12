556eb9361f4927684276517d2c918127c8b61f2e
NedHenry <ethenry@ucsc.edu>
2020-11-12 12:11:11 -0800

    fix bug throwing errors for works without filesets

--- app/indexers/representative_image_dimensions_index_behavior.rb
+++ app/indexers/representative_image_dimensions_index_behavior.rb
@@ -2,7 +2,7 @@ module RepresentativeImageDimensionsIndexBehavior
   extend ActiveSupport::Concern
 
   def index_representative_image_dimensions(doc)
-    file_set_id = object.thumbnail_id || Array(doc["hasRelatedImage_ssim"]).first
+    return doc unless (file_set_id = object.thumbnail_id || Array(doc["hasRelatedImage_ssim"]).first).present?
     fs = SolrDocument.find(file_set_id)
     doc["width_is"] = fs.width
     doc["height_is"] = fs.height
