1b4feb41003ae0dd6696fcf514b65efeac4ebc5a
NedHenry <ethenry@ucsc.edu>
2020-11-17 10:36:43 -0800

    improve automatic resaving job to work by collection instead of work type

--- app/jobs/resave_job.rb
+++ app/jobs/resave_job.rb
@@ -1,18 +1,15 @@
 class ResaveJob < Hyrax::ApplicationJob
-  def perform(work_types=nil)
+  def perform(collection_id=nil)
     rows = 100
-    work_types ||= ["Work","Course","Lecture"]
-    work_types.each do |work_type|
-      query = ActiveFedora::SolrQueryBuilder.construct_query_for_rel("has_model" => work_type)
-      num_works = ActiveFedora::SolrService.instance.conn.get(ActiveFedora::SolrService.select_path, params: { fq: query, rows: 0})["response"]["numFound"]    
-      start = 0
-      until start > num_works
-        works = ActiveFedora::SolrService.instance.conn.get(ActiveFedora::SolrService.select_path, params: { fq: query, rows: rows, start: start})["response"]["docs"]
-        works.each do  |wrk|
-          ActiveFedora::Base.find(wrk['id']).save
-        end
-        start = start + rows
+    query = ActiveFedora::SolrQueryBuilder.construct_query_for_rel("nesting_collection__ancestors" => collection_id )
+    num_works = ActiveFedora::SolrService.instance.conn.get(ActiveFedora::SolrService.select_path, params: { fq: query, rows: 0})["response"]["numFound"]
+    start = 0
+    until start > num_works
+      ActiveFedora::SolrService.instance.conn.get(ActiveFedora::SolrService.select_path, params: { fq: query, rows: rows, start: start})["response"]["docs"].each do  |doc|
+        next if doc["ancestor_collection_titles_ssim"].present?
+        ActiveFedora::Base.find(doc['id']).save
       end
+      start = start + rows
     end
   end
 end
