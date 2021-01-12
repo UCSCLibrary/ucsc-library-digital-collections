68d1e932216617ce092b95f245b2c05cc21b602c
NedHenry <ethenry@ucsc.edu>
2021-01-12 12:12:51 -0800

    pare down smoke tests to current basic funcitionality

--- spec/smoke/collection_spec.rb
+++ spec/smoke/collection_spec.rb
@@ -4,8 +4,8 @@ RSpec.feature 'Collection', js: false do
   context 'a guest user' do
     scenario do
       visit '/'
-      first('li.featured-item img').click
-      expect(page).to have_content "Collection Details"
+      first('ul#featured_collections>li img').click
+      expect(page).to have_content "Works In Collection"
       expect(page).to have_selector "div.document>div.thumbnail img"
     end
   end
