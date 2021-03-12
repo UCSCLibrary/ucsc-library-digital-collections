b5c5e26f7ddfc310056517d5d4be337600b9db99
Edward Henry <ethenry@ucsc.edu>
2021-03-12 15:15:37 -0800

    remove inline javascript, which doesn't work in the production env due to content security policy

--- app/assets/javascripts/facets.js
+++ app/assets/javascripts/facets.js
@@ -1,8 +1,20 @@
 $(document).on('turbolinks:load', function() {
-	$("button.facets-toggle").click(function() {
-		$(".facets-toggle span.text").text(function(i, text){
-			return text === "Show Filters" ? "Hide Filters" : "Show Filters";
-		});
-		$(".facets-toggle span.glyphicon").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+
+    $("button.facets-toggle").click(function() {
+	$(".facets-toggle span.text").text(function(i, text){
+	    return text === "Show Filters" ? "Hide Filters" : "Show Filters";
 	});
+	$(".facets-toggle span.glyphicon").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+    });
+
+    // This corrects some urls for the facet panel displayed
+    // on collection pages
+    $('#facets a.more_facets_link').each(function() {
+        url = $(this).attr('href');
+        $(this).attr('href', url.replace(/\/collections\/[a-zA-Z_]+\//i,"/catalog/"))
+    });
+
+    // In collection facet panels, set the keyword search attribute to 'q' instead of 'cq' so it displays properly
+    $('input#collection_search').attr('name','q')
+    
 });
