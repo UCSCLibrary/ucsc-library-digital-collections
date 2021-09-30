d57f9042ca12ae82a8d500c42ebd2411bedd9065
Benjamin Kiah Stroud <kiah@notch8.com>
2021-09-30 22:45:25 +0000

    i2 Split on pipe, fix rightsStatement mapping

--- app/models/concerns/bulkrax/metadata.rb
+++ app/models/concerns/bulkrax/metadata.rb
@@ -3,7 +3,7 @@ module Bulkrax
     extend ActiveSupport::Concern
 
     included do
-      property :bulkrax_identifier, predicate: ::RDF::URI.new("http://digitalcollections.library.ucsc.edu/resource#bulkraxIdentifier"), multiple: false do |index|
+      property :bulkrax_identifier, predicate: ::RDF::URI.new("http://digitalcollections.library.ucsc.edu/terms/bulkraxIdentifier"), multiple: false do |index|
         index.as :stored_searchable, :facetable
       end
     end
