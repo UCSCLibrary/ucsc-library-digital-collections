f5b05d8cd61c2cb0636ab5327ad8b744a2bae9e3
Ryan Schwab <rwschwab@ucsc.edu>
2021-10-11 15:43:49 -0700

    Silence deprecation warnings on production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.i18n.enforce_available_locales = false
 
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :silence
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
