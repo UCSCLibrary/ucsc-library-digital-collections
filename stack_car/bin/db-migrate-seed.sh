25ca0e77e5b5b3191b09cb65c4b98fab26ee1bfc
jzgo <jezreel.go@gmail.com>
2021-10-22 12:27:28 -0700

    really point at it

--- stack_car/bin/db-migrate-seed.sh
+++ stack_car/bin/db-migrate-seed.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-bin/db-wait.sh "$DB_HOST:$DB_PORT"
+stack_car/bin/db-wait.sh "$DB_HOST:$DB_PORT"
 bundle exec rails db:create
 bundle exec rails db:migrate
 
 if [ "$FCREPO_HOST" ]; then
-  bin/db-wait.sh "$FCREPO_HOST:$FCREPO_PORT"
+  stack_car/bin/db-wait.sh "$FCREPO_HOST:$FCREPO_PORT"
 fi
-bin/db-wait.sh "$SOLR_HOST:$SOLR_PORT"
+stack_car/bin/db-wait.sh "$SOLR_HOST:$SOLR_PORT"
 
 bundle exec rails db:seed
