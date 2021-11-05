547b649bef64c7cd6526e9dfd8c37034942e4ada
summer-cook <summercook@bellsouth.net>
2021-11-05 12:05:31 -0600

    remove nil value from datepicker input and add permitted params back

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,7 +1,6 @@
 class DatepickerInput < SimpleForm::Inputs::StringInput 
-  def input
-    # sets the default value to nil
-    input_html_options[:value] = nil
+  def input(value)
+    input_html_options[:value] = value
     # sets the input type to date to use the native rails datepicker
     input_html_options[:type] = "date"
     # allows a blank value to be included
