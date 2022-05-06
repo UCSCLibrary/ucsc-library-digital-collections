b37102701852c871471daaa689ce3cd39431c168
Benjamin Kiah Stroud <kiah@notch8.com>
2022-05-06 16:02:19 -0700

    null out controlled field values before assigning attrs

--- app/actors/hyrax/actors/work_actor.rb
+++ app/actors/hyrax/actors/work_actor.rb
@@ -3,6 +3,18 @@
 module Hyrax
   module Actors
     class WorkActor < Hyrax::Actors::BaseActor
+      private
+
+      def apply_save_data_to_curation_concern(env)
+        # OVERRIDE: Null out each controlled field before saving, meaning only
+        # incoming values will persist on the record. This results in blank
+        # controlled values in ingested CSVs being "deleted".
+        ::ScoobySnacks::METADATA_SCHEMA.controlled_field_names.each do |field_name|
+          env.curation_concern.public_send("#{field_name}=", [])
+        end
+
+        super
+      end
     end
   end
 end
