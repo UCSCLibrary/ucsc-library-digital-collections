af4170bcdddf80d7505d028494a98dd9a13e5643
Benjamin Kiah Stroud <kiah@notch8.com>
2022-05-09 16:18:52 -0700

    restrict which Work fields get nulled

--- app/actors/hyrax/actors/work_actor.rb
+++ app/actors/hyrax/actors/work_actor.rb
@@ -5,15 +5,24 @@ module Hyrax
     class WorkActor < Hyrax::Actors::BaseActor
       private
 
+      # OVERRIDE: Null out each controlled field before saving, meaning only
+      # incoming values will persist on the record. This results in blank
+      # controlled values in ingested CSVs being "deleted".
       def apply_save_data_to_curation_concern(env)
-        # OVERRIDE: Null out each controlled field before saving, meaning only
-        # incoming values will persist on the record. This results in blank
-        # controlled values in ingested CSVs being "deleted".
-        ::ScoobySnacks::METADATA_SCHEMA.controlled_field_names.each do |field_name|
-          env.curation_concern.public_send("#{field_name}=", [])
-        end
+        null_controlled_fields!(env)
 
         super
+      end
+
+      # OVERRIDE: Add custom method to make Bulkrax and ScoobySnacks more compatible
+      def null_controlled_fields!(env)
+        clean_attrs = clean_attributes(env.attributes)
+        ::ScoobySnacks::METADATA_SCHEMA.controlled_field_names.each do |field_name|
+          # do not null fields that are not being changed
+          next unless clean_attrs.keys.include?("#{field_name}_attributes")
+
+          env.curation_concern.public_send("#{field_name}=", [])
+        end
       end
     end
   end
