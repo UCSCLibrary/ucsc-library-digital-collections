8c6139b24861f98576836156fac51f67ad53add0
Sneha Gundurao <sgundura@ucsc.edu>
2022-08-18 13:23:12 -0700

    added comments

--- lib/ucsc/metadata_schema.rb
+++ lib/ucsc/metadata_schema.rb
@@ -6,7 +6,8 @@ module Ucsc
     end
 
      define_display_group_methods(custom_display_groups)
-
+    #TODO : move add_parent_value_display_field_names into ScoobySnacks/lib/metadata.rb
+    #Adding it under display groups just for convenience since add_parent_value_field_names methods have to be written on ScoobySnacks end
     def display_field_names
       super + admin_only_display_field_names + add_parent_value_display_field_names
     end
