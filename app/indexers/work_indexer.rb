bbb86329732fd7a8d7987536455a6d073c499982
Sneha Gundurao <sgundura@ucsc.edu>
2023-01-13 14:02:59 -0800

    modify comments

--- app/indexers/work_indexer.rb
+++ app/indexers/work_indexer.rb
@@ -104,7 +104,7 @@ class WorkIndexer < Hyrax::WorkIndexer
       parent_doc = SolrDocument.find(parent_work.id)
       # Loop through all inheritable fields
       ScoobySnacks::METADATA_SCHEMA.inheritable_fields.each do |field|
-        #if child value is present and ADD inheritance holds good, add parent value to the child value
+        #ADD inheritance
         if (solr_doc[field.solr_name]!= parent_doc[field.solr_name] && ScoobySnacks::METADATA_SCHEMA.add_parent_value_display_field_names.include?(field.name))
           solr_doc[field.solr_name].push(*parent_doc[field.solr_name])
         #commenting the below code and not removing it since we might need it for future use.  
@@ -129,7 +129,7 @@ class WorkIndexer < Hyrax::WorkIndexer
       # Loop through all collection inheritable fields
       ScoobySnacks::METADATA_SCHEMA.collection_inheritable_fields.each do |field|
         
-        #if child value is present and ADD inheritance holds good, add parent collection value to the child work 
+        #ADD inheritance 
         if (solr_doc[field.solr_name]!= parent_doc[field.solr_name] && ScoobySnacks::METADATA_SCHEMA.add_parent_value_display_field_names.include?(field.name))
           solr_doc[field.solr_name].push(*parent_doc[field.solr_name]) 
         #commenting the below code and not removing it since we might need it for future use. 
