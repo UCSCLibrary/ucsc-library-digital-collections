5ea6a012cf1145ddb7b99398e760620aea5604fd
Ryan Schwab <rwschwab@ucsc.edu>
2023-02-14 13:08:40 -0800

    Remove empty strings from RDF arrays, allow terms to be removed with edit form.

--- app/actors/hyrax/actors/work_actor.rb
+++ app/actors/hyrax/actors/work_actor.rb
@@ -9,10 +9,7 @@ module Hyrax
       # incoming values will persist on the record. This results in blank
       # controlled values in ingested CSVs being "deleted".
       def apply_save_data_to_curation_concern(env)
-        puts 'env.attributes: ' + env.attributes.inspect
-        puts 'env.curation_concern: ' + env.curation_concern.inspect
         null_controlled_fields!(env)
-
         super
       end
 
@@ -23,12 +20,15 @@ module Hyrax
 
           # do not null fields that are not being changed
           next unless clean_attrs.keys.include?("#{field_name}_attributes")
-          puts env.curation_concern["#{field_name}"].inspect
-          # do not null fields that are being removed
+
+          # Remove empty strings from curation_concern RDF arrays, we shouldn't have to do this...
+          no_empties = env.curation_concern["#{field_name}"].reject {|f| f.is_a?(String) && f.blank?}
+          env.curation_concern.public_send("#{field_name}=", no_empties)
+
+          # do not null fields if the _destroy property is set
           null_term = true
           clean_attrs["#{field_name}_attributes"].each do |term, properties|
             if properties.key?("_destroy") && !properties["_destroy"].blank?
-              puts 'not nulling ' + field_name
               null_term = false
               break
             end
