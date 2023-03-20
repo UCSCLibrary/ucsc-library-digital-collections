1740b3437270ee76bd2b96bce6cb30216187a0cd
Sneha Gundurao <sgundura@ucsc.edu>
2023-03-20 09:57:05 -0700

    386-fix facet panel bug

--- app/assets/javascripts/facets.js
+++ app/assets/javascripts/facets.js
@@ -1,17 +1,27 @@
 $(document).on('turbolinks:load', function() {
 
     $("button.facets-toggle").click(function() {
-	$(".facets-toggle span.text").text(function(i, text){
-	    return text === "Show Filters" ? "Hide Filters" : "Show Filters";
-	});
-	$(".facets-toggle span.glyphicon").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+        $(".facets-toggle span.text").text(function(i, text) {
+            return text === "Show Filters" ? "Hide Filters" : "Show Filters";
+        });
+        $(".facets-toggle span.glyphicon").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
     });
 
     // This corrects some urls for the facet panel displayed
     // on collection pages
+
+
+    $('#facets a.facet_select').each(function() {
+        url = $(this).attr('href');
+        queryParam = url.split('?')[1]
+            // $(this).attr('href', url.replace(/\/collections\/[a-zA-Z_]+\//i, "/catalog/"))
+    });
+
     $('#facets a.more_facets_link').each(function() {
         url = $(this).attr('href');
-        $(this).attr('href', url.replace(/\/collections\/[a-zA-Z_]+\//i,"/catalog/"))
+        urlParam = url.replace(/\/collections\/[a-zA-Z_]+\//i, "/catalog/")
+        finalUrl = urlParam + queryParam
+        $(this).attr('href', finalUrl)
     });
-    
+
 });
