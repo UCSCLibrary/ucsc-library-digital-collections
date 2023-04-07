1f2d998bb6e17f7e8dddec89fd3bb4aaa9a7c519
Sneha Gundurao <sgundura@ucsc.edu>
2023-04-07 14:27:08 -0700

    remove local=en

--- app/assets/javascripts/facets.js
+++ app/assets/javascripts/facets.js
@@ -20,8 +20,12 @@ $(document).on('turbolinks:load', function() {
     $('#facets a.more_facets_link').each(function() {
         url = $(this).attr('href');
         urlParam = url.replace(/\/collections\/[a-zA-Z_]+\//i, "/catalog/")
-        finalUrl = urlParam + queryParam
-        finalUrl.replace("locale=en", "");
+        pArray = queryParam.split('&');
+        locIndex = pArray.indexOf('locale=en');
+        if (locIndex > -1) {
+            pArray.splice(locIndex, 1);
+        }
+        finalUrl = urlParam + pArray.join('&');
         $(this).attr('href', finalUrl)
     });
 
