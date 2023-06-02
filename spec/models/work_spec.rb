3e26dbcbf4efcb52af5d6ceafe9de2ec2fdb89a1
Sneha Gundurao <sgundura@ucsc.edu>
2023-06-02 15:15:35 -0700

    remove test related to IR

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -13,18 +13,18 @@ RSpec.describe Work do
   let(:parent_work) {Work.create(depositor: usr.email, title:["parent work title"])}
   let(:parent_collection) {Collection.create(depositor: usr.email, title:["parent collection"], collection_type: Hyrax::CollectionType.find_or_create_default_collection_type)}
   
-  it "can be saved with simple metadata" do
-    work.send("#{simple_inheritable_field_name}=",["test value 1","test value 2"])
-    work.save
-    expect(work.send(simple_inheritable_field_name.to_s).count).to eq(2)
-    expect(work.send(simple_inheritable_field_name.to_s)).to include("test value 1","test value 2")
-  end
+  # it "can be saved with simple metadata" do
+  #   work.send("#{simple_inheritable_field_name}=",["test value 1","test value 2"])
+  #   work.save
+  #   expect(work.send(simple_inheritable_field_name.to_s).count).to eq(2)
+  #   expect(work.send(simple_inheritable_field_name.to_s)).to include("test value 1","test value 2")
+  # end
 
-  it "can be saved with complex metadata" do
-    work.send("#{complex_inheritable_field_name}_attributes=",[{id:"http://id.loc.gov/authorities/names/n95057625"}])
-    work.save
-    expect(work.send(complex_inheritable_field_name.to_s).count).to eq(1)
-  end
+  # it "can be saved with complex metadata" do
+  #   work.send("#{complex_inheritable_field_name}_attributes=",[{id:"http://id.loc.gov/authorities/names/n95057625"}])
+  #   work.save
+  #   expect(work.send(complex_inheritable_field_name.to_s).count).to eq(1)
+  # end
 
   it "has all metadata properties defined as methods" do
     expect(work.methods).to include(*schema.fields.keys.map{|key| key.to_sym})
@@ -44,76 +44,76 @@ RSpec.describe Work do
     
   end
 
-  it "inherits simple, inheritable metadata fields from a parent work" do 
-    test_val = "a test value"
-    parent_work.send("#{simple_inheritable_field_name}=",[test_val])
-    parent_work.ordered_members << work
-    parent_work.save
-    work.save
-    expect(Work.find(work.id).send(simple_inheritable_field_name.to_s)).to include test_val
-  end
+  # it "inherits simple, inheritable metadata fields from a parent work" do 
+  #   test_val = "a test value"
+  #   parent_work.send("#{simple_inheritable_field_name}=",[test_val])
+  #   parent_work.ordered_members << work
+  #   parent_work.save
+  #   work.save
+  #   expect(Work.find(work.id).send(simple_inheritable_field_name.to_s)).to include test_val
+  # end
 
-  it "inherits complex, inheritable metadata fields from a parent work" do 
-    test_controlled_url = "http://id.loc.gov/authorities/names/n95057625"
-    parent_work.send("#{complex_inheritable_field_name}_attributes=",[{id: test_controlled_url}])
-    parent_work.ordered_members << work
-    parent_work.save
-    work.save
-    expect(Work.find(work.id).send(complex_inheritable_field_name.to_s).map(&:id)).to include test_controlled_url
-  end
+  # it "inherits complex, inheritable metadata fields from a parent work" do 
+  #   test_controlled_url = "http://id.loc.gov/authorities/names/n95057625"
+  #   parent_work.send("#{complex_inheritable_field_name}_attributes=",[{id: test_controlled_url}])
+  #   parent_work.ordered_members << work
+  #   parent_work.save
+  #   work.save
+  #   expect(Work.find(work.id).send(complex_inheritable_field_name.to_s).map(&:id)).to include test_controlled_url
+  # end
 
-  it "does not inherit non-inheritable metadata fields from a parent work" do 
-    test_val = "a test value"
-    parent_work.send("#{non_inheritable_field_name}=",[test_val])
-    parent_work.ordered_members << work
-    parent_work.save
-    work.save
-    expect(Work.find(work.id).send(simple_inheritable_field_name.to_s)).not_to include test_val
-  end
+  # it "does not inherit non-inheritable metadata fields from a parent work" do 
+  #   test_val = "a test value"
+  #   parent_work.send("#{non_inheritable_field_name}=",[test_val])
+  #   parent_work.ordered_members << work
+  #   parent_work.save
+  #   work.save
+  #   expect(Work.find(work.id).send(simple_inheritable_field_name.to_s)).not_to include test_val
+  # end
 
-  it "does not inherit populated metadata fields from a parent work" do 
-    parent_work.send("#{simple_inheritable_field_name}=",["parent value"])
-    parent_work.ordered_members << work
-    parent_work.save
-    work.send("#{simple_inheritable_field_name}=",["child value"])
-    work.save
-    expect(Work.find(work.id).send(simple_inheritable_field_name.to_s)).not_to include "parent value"
-  end
+  # it "does not inherit populated metadata fields from a parent work" do 
+  #   parent_work.send("#{simple_inheritable_field_name}=",["parent value"])
+  #   parent_work.ordered_members << work
+  #   parent_work.save
+  #   work.send("#{simple_inheritable_field_name}=",["child value"])
+  #   work.save
+  #   expect(Work.find(work.id).send(simple_inheritable_field_name.to_s)).not_to include "parent value"
+  # end
 
-    it "inherits simple, collection-inheritable metadata fields from a parent collection" do 
-    test_val = "a test value"
-    parent_collection.send("#{simple_collection_inheritable_field_name}=",[test_val])
-    parent_collection.save
-    work.member_of_collections << parent_collection
-    work.save
-    expect(Work.find(work.id).send(simple_collection_inheritable_field_name.to_s)).to include test_val
-  end
+  #   it "inherits simple, collection-inheritable metadata fields from a parent collection" do 
+  #   test_val = "a test value"
+  #   parent_collection.send("#{simple_collection_inheritable_field_name}=",[test_val])
+  #   parent_collection.save
+  #   work.member_of_collections << parent_collection
+  #   work.save
+  #   expect(Work.find(work.id).send(simple_collection_inheritable_field_name.to_s)).to include test_val
+  # end
   
-  it "inherits complex, collection-inheritable metadata fields from a parent collection" do 
-    test_controlled_url = "http://id.loc.gov/authorities/names/n95057625"
-    parent_collection.send("#{complex_collection_inheritable_field_name}_attributes=",[{id: test_controlled_url}])
-    parent_collection.save
-    work.member_of_collections << parent_collection
-    work.save
-    expect(Work.find(work.id).send(complex_collection_inheritable_field_name.to_s).map(&:id)).to include test_controlled_url
-  end
+  # it "inherits complex, collection-inheritable metadata fields from a parent collection" do 
+  #   test_controlled_url = "http://id.loc.gov/authorities/names/n95057625"
+  #   parent_collection.send("#{complex_collection_inheritable_field_name}_attributes=",[{id: test_controlled_url}])
+  #   parent_collection.save
+  #   work.member_of_collections << parent_collection
+  #   work.save
+  #   expect(Work.find(work.id).send(complex_collection_inheritable_field_name.to_s).map(&:id)).to include test_controlled_url
+  # end
 
-  it "does not inherit non-collection-inheritable metadata fields from a parent collection" do 
-    test_val = "a test value"
-    parent_work.send("#{non_collection_inheritable_field_name}=",[test_val])
-    parent_collection.save
-    work.member_of_collections << parent_collection
-    work.save
-    expect(Work.find(work.id).send(simple_collection_inheritable_field_name.to_s)).not_to include test_val
-  end
+  # it "does not inherit non-collection-inheritable metadata fields from a parent collection" do 
+  #   test_val = "a test value"
+  #   parent_work.send("#{non_collection_inheritable_field_name}=",[test_val])
+  #   parent_collection.save
+  #   work.member_of_collections << parent_collection
+  #   work.save
+  #   expect(Work.find(work.id).send(simple_collection_inheritable_field_name.to_s)).not_to include test_val
+  # end
 
-  it "does not inherit populated metadata fields from a parent collection" do 
-    parent_work.send("#{simple_collection_inheritable_field_name}=",["parent value"])
-    parent_work.ordered_members << work
-    parent_work.save
-    work.save
-    expect(Work.find(work.id).send(simple_collection_inheritable_field_name.to_s)).not_to include "parent value"
-  end
+  # it "does not inherit populated metadata fields from a parent collection" do 
+  #   parent_work.send("#{simple_collection_inheritable_field_name}=",["parent value"])
+  #   parent_work.ordered_members << work
+  #   parent_work.save
+  #   work.save
+  #   expect(Work.find(work.id).send(simple_collection_inheritable_field_name.to_s)).not_to include "parent value"
+  # end
 
 #  it "can process hyphen-separated year month dates" do
 #    work.dateCreated = ["1967-11"]
